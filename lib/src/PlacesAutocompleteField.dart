467d63932ea5db3f9b86e4632b215616093f1b27
Daniel Morawetz <moro571@gmail.com>
2018-09-11 11:33:26 +0200

    Clear up missconception about offset

--- lib/src/PlacesAutocompleteField.dart
+++ lib/src/PlacesAutocompleteField.dart
@@ -91,7 +91,18 @@ class PlacesAutocompleteField extends StatefulWidget {
   /// extra padding introduced by the decoration to save space for the labels).
   final InputDecoration inputDecoration;
 
-  /// Min length of input for autocomplete to start.
+  /// The position, in the input term, of the last character that the service
+  /// uses to match predictions.
+  ///
+  /// For example, if the input is 'Google' and the
+  /// offset is 3, the service will match on 'Goo'. The string determined by the
+  /// offset is matched against the first word in the input term only. For
+  /// example, if the input term is 'Google abc' and the offset is 3, the service
+  /// will attempt to match against 'Goo abc'. If no offset is supplied, the
+  /// service will use the whole term. The offset should generally be set to the
+  /// position of the text caret.
+  ///
+  /// Source: https://developers.google.com/places/web-service/autocomplete
   final num offset;
 
   final Mode mode;
