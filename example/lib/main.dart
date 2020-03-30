fd7e643a08f422bf356776095bc507e1d2c357dd
Jawad Ahmed <147865ba@gmail.com>
2020-03-30 17:01:07 +0500

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-const kGoogleApiKey = "AIzaSyD2hIWbksN1_QucfircwEyXUMqJfKZgw5w";
+const kGoogleApiKey = "API_KEy";
 
 // to get places detail (lat/lng)
 GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
