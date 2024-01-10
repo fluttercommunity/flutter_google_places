6191f18df6a625b975927fca9bf24e3b9560cfa9
arjun-c-369 <62787419+arjun-c-369@users.noreply.github.com>
2024-01-10 15:46:09 +0530

    Update places_autocomplete_form_field.dart

--- lib/src/places_autocomplete_form_field.dart
+++ lib/src/places_autocomplete_form_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_google_places/src/flutter_google_places.dart';
 import 'package:flutter_google_places/src/places_autocomplete_field.dart';
-import 'package:google_maps_webservice/places.dart';
+import 'package:google_maps_webservice_ex/places.dart';
 
 /// A [FormField] that contains a [PlacesAutocompleteField].
 ///
